282e50cabc5eb88f8f3b0bfb1fcce6fcdd6051c3
Ryan Frame <code@ryanjframe.com>
2021-10-13 15:55:43 -0400

    Add license file, notices

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,8 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 //! HTTP client wrappers.
 //!
 //! Errors from the backend HTTP client are passed to user code, so dealing with
